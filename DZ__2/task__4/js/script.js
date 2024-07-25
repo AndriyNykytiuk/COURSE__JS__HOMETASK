56c809af66d9e2aa92786d00c7c6fa7392a7f71c
AndriyNykytiuk <andrijnykytiyk@gmail.com>
2024-07-25 15:55:08 +0300

    Update script.js

--- DZ__2/task__4/js/script.js
+++ DZ__2/task__4/js/script.js
@@ -4,4 +4,4 @@ const KILOMETR=100000 // стільки сантиметрів у 1 кіломе
 let lenghtMeter = lenghtCentimetr / METER
 let lenghtKm = lenghtCentimetr / KILOMETR
 
-document.write(`<h2>ви ввели ${lenghtCentimetr} сантиметрів, а це ${lenghtMeter.toFixed(2)} метрів та ${lenghtKm.toFixed(2)} кілометрів</h2>`)
+document.write(`<h2>ви ввели ${lenghtCentimetr} сантиметрів, а це ${lenghtMeter.toFixed(2)} метрів та ${lenghtKm.toFixed(3)} кілометрів</h2>`)
