5e3e7ef73d2efb1a89b4378d683e5839ac6176e7
AndriyNykytiuk <andrijnykytiyk@gmail.com>
2024-08-06 16:19:51 +0300

    Delete DZ__5 directory

--- DZ__5/task__0/js/script.js
+++
@@ -1,4 +0,0 @@
-
-for (let button = 1; button <= 8; button++){
-	document.write(`${button} - <button> Hello !</button> <br> <br>`)
-}
--- DZ__5/task__2/js/script.js
+++
@@ -1,3 +0,0 @@
-for (let area = 1; area <= 10; area++){
-	document.write(`Product #${area} <input> <br> <br>`)
-}
