0b3c389fa9744730f940c3fe56d8ff8f6aceb36b
AndriyNykytiuk <andrijnykytiyk@gmail.com>
2024-08-13 14:18:18 +0300

    Delete DZ__7 directory

--- DZ__7/task__7/js/script.js
+++
@@ -1,8 +0,0 @@
-function randomImage() {
-	
-	let randomNumber = Math.floor(Math.random() * 4)
-	
-	document.write(`<img src="img/${randomNumber}.webp" alt="#" width=250px height=300px > `);
-}
-
-randomImage()
